1def9938c038560891ecb824d4b2bf70da2f38f7
Oskar <oskar.cahill@maps.org.uk>
2025-10-31 09:58:35 +0000

    pipline fixes

--- netlify/github-scripts/shared/run-lint-checks.sh
+++ netlify/github-scripts/shared/run-lint-checks.sh
@@ -4,9 +4,9 @@ set -eu
 # Convert template parameter to Bash variable
 COMPARE_WITH_TARGET="${COMPARE_WITH_TARGET,,}"  # lowercase
 echo "COMPARE_WITH_TARGET = '$COMPARE_WITH_TARGET'"
-echo "runAll= '$RUN_ALL'"
+echo "runAll= '$RUN_ALL_SONAR'"
 
-if [ "$COMPARE_WITH_TARGET" = "true" ] || [ "$RUN_ALL" = "true" ]; then
+if [ "$COMPARE_WITH_TARGET" = "true" ] || [ "$RUN_ALL_SONAR" = "true" ]; then
     npx nx run-many -t lint
 else
     npx nx affected:lint --base=HEAD~1 --head=HEAD
--- netlify/github-scripts/shared/run-typecheck.sh
+++ netlify/github-scripts/shared/run-typecheck.sh
@@ -3,9 +3,9 @@ set -eu
 
 # Convert template parameter to Bash variable
 COMPARE_WITH_TARGET="${COMPARE_WITH_TARGET_ENV,,}"  # lowercase
-echo "runAll= '$RUN_ALL'"
+echo "runAll= '$RUN_ALL_SONAR'"
 
-if [ "$COMPARE_WITH_TARGET" = "true" ] || [ "$RUN_ALL" = "true" ]; then
+if [ "$COMPARE_WITH_TARGET" = "true" ] || [ "$RUN_ALL_SONAR" = "true" ]; then
     npx nx run-many -t typecheck
 else
     npx nx affected --target=typecheck --base=HEAD~1 --head=HEAD
